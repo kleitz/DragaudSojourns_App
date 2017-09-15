0499af97daacd8267eb22e736dc47d21eae7e875
Josh Van Natta <jjvannatta88@gmail.com>
2017-09-14 23:19:54 -0600

    Modified incorrect footer menu pointers

--- resources/views/layouts/public/footer.blade.php
+++ resources/views/layouts/public/footer.blade.php
@@ -1,12 +1,12 @@
 <div class="footer2">
 
   <ul>
-  	<li ><a href="index.html">Home</a></li>
-  	<li ><a href="studenttours.html">Student Tours</a></li>
-      <li ><a href="privatetours.html">Private Tours</a></li>
-  	<li><a href="testimonials.html">Testimonials</a></li>
-  	<li><a href="aboutus.html">About Us</a></li>
-  <li><a href="contactus.html">Contact Us</a></li>
+  	<li ><a href="{{ url('/') }}">Home</a></li>
+  	<li ><a href="{{ url('/studenttours') }}">Student Tours</a></li>
+      <li ><a href="{{ url('/privatetours') }}">Private Tours</a></li>
+  	<li><a href="{{ url('/testimonials') }}">Testimonials</a></li>
+  	<li><a href="{{ url('/aboutus') }}">About Us</a></li>
+  <li><a href="{{ url('/contactus') }}">Contact Us</a></li>
   </ul>
 
   <img src="{{ url('/assets/public/img/dragoo-Bottom-clear_vectorized_gradient.png') }}"/>
