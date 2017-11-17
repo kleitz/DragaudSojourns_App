dbad7cafea62a464b2a1a3481220db97d3fcd215
Josh Van Natta <jjvannatta88@gmail.com>
2017-11-17 09:17:42 -0700

    Finalized all payments date range calculator

--- resources/views/partials/admin/pagebottom.blade.php
+++ resources/views/partials/admin/pagebottom.blade.php
@@ -2,12 +2,12 @@
   @if ($numPages > 1)
   <div>
     @if ($curPage > 1)
-    <a href="/admin/{{ $authAdmin }}/{{ $curSection }}/{{ $curPage - 1 . $search}}">Previous page</a>
+    <a href="/admin/{{ $authAdmin }}/{{ $curSection }}/{{ $curPage - 1 . $search . $dateFromSearch . $dateToSearch  }}">Previous page</a>
     @endif
   </div>
   <div>
     @if ($curPage != $numPages)
-    <a href="/admin/{{ $authAdmin }}/{{ $curSection }}/{{ $curPage + 1 . $search}}">Next page</a>
+    <a href="/admin/{{ $authAdmin }}/{{ $curSection }}/{{ $curPage + 1 . $search . $dateFromSearch . $dateToSearch  }}">Next page</a>
     @endif
   </div>
   @endif
